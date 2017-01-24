27a65f983cd27779d0b4578d25e691ad240ca14d
Starups <stan.peters@live.nl>
2017-01-24 13:58:29 +0100

    Lobby

--- src/Server/Lobby.java
+++ src/Server/Lobby.java
@@ -6,4 +6,16 @@ import java.util.ArrayList;
 public class Lobby {
 	//TODO putplayer getplayer arraylist
 	private List<String> players = new ArrayList<String>();
+
+	public void putPlayer(String name){
+		players.add(name);
+	}
+
+	public List<String> getPlayer(){
+		return players;
+	}
+
+	public void clearLobby(){
+		players.clear();
+	}
 }
